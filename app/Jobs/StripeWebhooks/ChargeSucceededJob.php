31e5739b6b415d0077bca453806f7f92ce43ee8f
artoodetoo <i.am@artoodetoo.org>
2020-02-03 22:23:03 +0500

    Fix Payment class reference in the job

--- app/Jobs/StripeWebhooks/ChargeSucceededJob.php
+++ app/Jobs/StripeWebhooks/ChargeSucceededJob.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Spatie\WebhookClient\Models\WebhookCall;
+use App\Payment;
 
 class ChargeSucceededJob implements ShouldQueue
 {
